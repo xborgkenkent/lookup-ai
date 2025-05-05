96ab51d9a9555c24311481f52575909af0d1a92b
Kent Regiel Buncal <kentregiel.buncal139@gmail.com>
2025-05-05 14:14:41 +0800

    added restaurant response model

---
+++ backend/models/restaurant_models.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
+ 
+class RestaurantResponse(BaseModel):
+    name: str
+    rating: Optional[float] = None
+    price: Optional[float] = None
+    hours: Optional[str] = None
+    address: Optional[str] = None
+    cuisine: Optional[str] = None
+    fsq_id: str
+    
